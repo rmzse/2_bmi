69d7b661f2dac495066350c5ee55dfbe20a241a3
rmzse <rodrigo@therawdelicious.com>
2016-11-22 19:15:15 +0100

    include all metric tests in a single context

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,26 +1,28 @@
 describe ("Person", function() {
   var person;
 
-  beforeEach(function () {
-    person = new Person({weight: 90, height: 186});
-  });
+  describe("calculate BMI with metric units", function(){
+    beforeEach(function () {
+      person = new Person({weight: 90, height: 186});
+    });
 
-  it("has a weight of 90", function() {
-    expect(person.weight).toEqual(90);
-  });
 
-  it("has a height of 186", function() {
-    expect(person.height).toEqual(186);
-  });
+    it("has a weight of 90", function() {
+      expect(person.weight).toEqual(90);
+    });
 
-  it("should calculate a BMI value", function (){
-    person.calculate_bmi();
-    expect(person.bmiValue).toEqual(26.01);
-  });
+    it("has a height of 186", function() {
+      expect(person.height).toEqual(186);
+    });
 
-  it("should have a BMI message", function(){
-    person.calculate_bmi();
-    expect(person.bmiMessage).toEqual("Overweight");
-  });
+    it("should calculate a BMI value", function (){
+      person.calculate_bmi();
+      expect(person.bmiValue).toEqual(26.01);
+    });
 
+    it("should have a BMI message", function(){
+      person.calculate_bmi();
+      expect(person.bmiMessage).toEqual("Overweight");
+    });
+  });
 });
