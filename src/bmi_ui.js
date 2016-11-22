d03693bb687379801464e5974a39933f221cd031
rmzse <rodrigo@therawdelicious.com>
2016-11-22 22:21:14 +0100

    rollback on all input type radio entries in all ui files

--- src/bmi_ui.js
+++ src/bmi_ui.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('#calculate').click(function () {
     var w = $('#weight').val();
     var h = $('#height').val();
-    var t = $("input[type='radio'].toggle:checked").val();
+    // var t = $("input[type='radio'].toggle:checked").val();
     var person = new Person({weight: w, height: h});
     person.calculate_bmi();
     $('#display_value').html('Your BMI is ' + person.bmiValue);
