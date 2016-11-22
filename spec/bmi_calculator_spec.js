7ac9470213091da838f9b55a71b6ed4103e2e97f
rmzse <rodrigo@therawdelicious.com>
2016-11-22 21:43:35 +0100

    add imperial calculation (redundant) to bmi calc spec

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -17,16 +17,16 @@ describe ("BMICalculator", function(){
   });
 
 
-  // describe("calculates value with imperial units", function(){
-  //   beforeEach(function(){
-  //     person = new Person({weight: 200, height: 66, toggle: true});
-  //     calculate = new BMICalculator();
-  //   });
-  //
-  //   it(" and returns the imperial BMI value", function(){
-  //     calculate.bmi(person);
-  //     expect(person.bmiValue).toEqual(32.28);
-  //   });
-  // });
+  describe("calculates value with imperial units", function(){
+    beforeEach(function(){
+      person = new Person({weight: 200, height: 66, toggle: 'true'});
+      calculate = new BMICalculator();
+    });
+
+    it(" and returns the imperial BMI value", function(){
+      calculate.bmi(person);
+      expect(person.bmiValue).toEqual(32.28);
+    });
+  });
 
 });
