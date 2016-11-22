a9a03f7ce3ce4128d5c06332f12bd5bb275d6668
rmzse <rodrigo@therawdelicious.com>
2016-11-22 22:18:40 +0100

    add toggle var init to ui implementation

--- src/bmi_ui.js
+++ src/bmi_ui.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   $('#calculate').click(function () {
-    var w = parseFloat($('#weight').val());
-    var h = parseFloat($('#height').val());
+    var w = $('#weight').val();
+    var h = $('#height').val();
+    var t = $("input[type='radio'].toggle:checked").val();
     var person = new Person({weight: w, height: h});
     person.calculate_bmi();
     $('#display_value').html('Your BMI is ' + person.bmiValue);
