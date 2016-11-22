46fab56748f40f0e15856e4a9142a3880bf50868
rmzse <rodrigo@therawdelicious.com>
2016-11-22 19:17:23 +0100

    duplicate metric unit tests to create imperial unit tests

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -6,7 +6,6 @@ describe ("Person", function() {
       person = new Person({weight: 90, height: 186});
     });
 
-
     it("has a weight of 90", function() {
       expect(person.weight).toEqual(90);
     });
@@ -23,6 +22,30 @@ describe ("Person", function() {
     it("should have a BMI message", function(){
       person.calculate_bmi();
       expect(person.bmiMessage).toEqual("Overweight");
+    });
+  });
+
+  describe("calculate BMI with imperial units", function(){
+    beforeEach(function () {
+      person = new Person({weight: 200, height: 66});
+    });
+
+    it("has a weight of 90", function() {
+      expect(person.weight).toEqual(200);
+    });
+
+    it("has a height of 186", function() {
+      expect(person.height).toEqual(66);
+    });
+
+    it("should calculate a BMI value", function (){
+      person.calculate_bmi();
+      expect(person.bmiValue).toEqual(32.28);
+    });
+
+    it("should have a BMI message", function(){
+      person.calculate_bmi();
+      expect(person.bmiMessage).toEqual("Obese");
     });
   });
 });
