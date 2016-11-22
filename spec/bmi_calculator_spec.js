c575adf40f7add45b74cbdc467c7b1bf6055dfb2
rmzse <rodrigo@therawdelicious.com>
2016-11-22 18:43:12 +0100

    roll back to just keep the person spec with one test

--- spec/bmi_calculator_spec.js
+++ spec/bmi_calculator_spec.js
@@ -2,15 +2,15 @@ describe ("BMICalculator", function(){
   var calculator;
   var person;
 
-  beforeEach(function(){
-    person = new Person({weight: 90, height: 186});
-    calculator = new BMICalculator();
-  });
-
-  it("calculates BMI for a person using metric units", function(){
-    calculator.bmi(person);
-    expect(person.bmiValue).toEqual(26.01);
-  });
+  // beforeEach(function(){
+  //   person = new Person({weight: 90, height: 186});
+  //   calculate = new BMICalculator();
+  // });
+  // 
+  // it("calculates BMI for a person using metric units", function(){
+  //   calculate.bmi(person);
+  //   expect(person.bmiValue).toEqual(26.01);
+  // });
 
   // describe("calculates value with imperial units and", function(){
   //   beforeEach(function(){
