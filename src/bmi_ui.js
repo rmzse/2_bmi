427c08a21196f25cf7dcc4390036fbfa6e3c05f5
rmzse <rodrigo@therawdelicious.com>
2016-11-24 07:01:58 +0100

    check if toggle is selected otherwise alert about this

--- src/bmi_ui.js
+++ src/bmi_ui.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-  var w;
-  var h;
-  var t;
-
   $('#calculate').click(function () {
-    // var w = $('#weight').val();
+    if ( $('input[name=toggle]:checked').val() === undefined ) alert('You need to choose metric or imperial units first!');
 
-    h = $('#height').val();
-    t = $('input[name=toggle]:checked').val();
+    var w = parseInt($('#mweight').val());
+    var h = parseInt($('#height').val());
+    var t = $('input[name=toggle]:checked').val();
+    if ( t == 'true') {
+      w = parseInt($('#iweight').val());
+      h = parseInt($('#ft').val()) * 12 + parseInt($('#in').val());
+    }
     var person = new Person({weight: w, height: h, toggle: t});
     person.calculate_bmi();
     $('#display_value').html('Your BMI is ' + person.bmiValue);
