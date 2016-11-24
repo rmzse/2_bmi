98aeb69e8776c2329264e2e817979c3f395fe7a4
rmzse <rodrigo@therawdelicious.com>
2016-11-24 07:11:13 +0100

    correct the control flow of the alert, to create a loop when toggle is empty, but without loop method while that throws you into an infinite loop

--- src/bmi_ui.js
+++ src/bmi_ui.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
   $('#calculate').click(function () {
-    if ( $('input[name=toggle]:checked').val() === undefined ) alert('You need to choose metric or imperial units first!');
-
     var w = parseInt($('#mweight').val());
     var h = parseInt($('#height').val());
     var t = $('input[name=toggle]:checked').val();
     if ( t == 'true') {
       w = parseInt($('#iweight').val());
-      h = parseInt($('#ft').val()) * 12 + parseInt($('#in').val()); 
+      h = parseInt($('#ft').val()) * 12 + parseInt($('#in').val());
     }
     var person = new Person({weight: w, height: h, toggle: t});
     person.calculate_bmi();
-    $('#display_value').html('Your BMI is ' + person.bmiValue);
-    $('#display_message').html('and you are '+ person.bmiMessage);
+    if ( $('input[name=toggle]:checked').val() === undefined ) {
+      alert('You need to choose metric or imperial units first!');
+    }
+    else {
+      $('#display_value').html('Your BMI is ' + person.bmiValue);
+      $('#display_message').html('and you are '+ person.bmiMessage);
+    }
   });
 });
