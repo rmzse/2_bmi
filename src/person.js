2ebb9be307e64b3221a2dd9936ba29b602255331
rmzse <rodrigo@therawdelicious.com>
2016-11-24 07:31:15 +0100

    clean up all code from hold-my-hand type of comments from production

--- src/person.js
+++ src/person.js
@@ -2,8 +2,6 @@ function Person(attr){
   this.weight = attr.weight;
   this.height = attr.height;
   this.toggle = attr.toggle;
-  // console.log('person object initiated with ' +
-  // 'w: ' + this.weight + ', h: ' + this.height + ', toggle: ' + this.toggle);
 
   Person.prototype.calculate_bmi = function () {
     calculate = new BMICalculator();
